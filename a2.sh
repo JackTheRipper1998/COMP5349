a6cc837148631d1be7969f7a35a939b5ff1e9e60
JackTheRipper1998 <809484889@qq.com>
2022-05-27 10:17:52 +0800

    wefewf

--- a2.sh
+++ a2.sh
@@ -1,10 +1,11 @@
 spark-submit \
       --master yarn \
       --deploy-mode client \
-       CUAD_data_spark_processing.py \
       -- num-executors 3 \
       -- executor-cores 8 \
-      -- executor-memory 12G
+      -- executor-memory 2G \
+       CUAD_data_spark_processing.py 
+
 	
 
 
